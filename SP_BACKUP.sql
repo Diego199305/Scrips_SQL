dce4aa46cce2f68dc26a55d486fe6331d34157ae
Diego199305 <140246395+Diego199305@users.noreply.github.com>
2023-10-01 07:58:57 -0300

    Update SP_BACKUP.sql

--- SP_BACKUP.sql
+++ SP_BACKUP.sql
@@ -2,29 +2,30 @@
 -- Com o parâmetro @op será indicado o tipo de backup que deseja realizar.
 -- Se o parâmetro indicado for incorreto, a operação será inválida.
 
-CREATE PROC SP_BACKUP (@op CHAR(1), @db NVARCHAR(20))
+CREATE PROC [dbo].[SP_BACKUP] (@op CHAR(1), @db NVARCHAR(20))
 AS
 BEGIN
-DECLARE @DATA VARCHAR(25), @ext CHAR(4), @cam NVARCHAR(50)
+DECLARE @DATA VARCHAR(25), @ext CHAR(4), @cam NVARCHAR(50), @hora varchar(10)
 SET @DATA = CONVERT(VARCHAR(25), GETDATE(), 104)
-SET @cam = 'C:\Backup\'
+set @hora = convert(nvarchar, datepart(hour, getdate())) + '_' + convert(nvarchar, datepart(minute, getdate())) + '_' + convert(nvarchar, datepart(second, getdate()))
+SET @cam = 'C:\Backup_HML\'
 IF @op = 'F'
 	BEGIN
 	DECLARE @sql NVARCHAR(250)
 	SET @ext = '.bak' 
-	SET @sql = CONCAT('BACKUP DATABASE ', @db, ' TO DISK = N''',@cam, @db, '_FULL_', @DATA, @ext,'''', ' WITH COMPRESSION, CHECKSUM')
+	SET @sql = CONCAT('BACKUP DATABASE ', @db, ' TO DISK = N''',@cam, @db, '_FULL_', @DATA, @hora, @ext,'''', ' WITH COMPRESSION, CHECKSUM, NOINIT')
 	EXEC sp_executesql @sql 
 	END
 ELSE IF @op = 'D'
 	BEGIN
 	SET @ext = '.bak' 
-	SET @sql = CONCAT('BACKUP DATABASE ', @db, ' TO DISK = N''',@cam, @db,'_DIFF_', @DATA, @ext,'''', ' WITH DIFFERENTIAL, COMPRESSION, CHECKSUM')
+	SET @sql = CONCAT('BACKUP DATABASE ', @db, ' TO DISK = N''',@cam, @db,'_DIFF_', @DATA, @hora, @ext,'''', ' WITH DIFFERENTIAL, COMPRESSION, CHECKSUM')
 	EXEC sp_executesql @sql 
 	END
 ELSE IF @op = 'L'
 	BEGIN
 	SET @ext = '.bak' 
-	SET @sql = CONCAT('BACKUP LOG ', @db, ' TO DISK = N''',@cam, @db, '_LOG_', @DATA, @ext,'''', ' WITH COMPRESSION, CHECKSUM')
+	SET @sql = CONCAT('BACKUP LOG ', @db, ' TO DISK = N''',@cam, @db, '_LOG_', @DATA, @hora, @ext,'''', ' WITH COMPRESSION, CHECKSUM')
 	EXEC sp_executesql @sql 
 	END
 ELSE 
